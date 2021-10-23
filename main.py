8f072426c3aae9323e55828127933283e3b6d353
rosadsky <rosadsky@hotmail.com>
2021-10-23 15:47:11 +0200

    finding positions and minimum possible moves

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def EulerovKon(chessboard_size):
         print("")
 
     #checkHorseInChessboard(chessboard_size, position_x, position_y, x_move, y_move)
-    windstoffAlgorhithm(starting_position_x,starting_position_y,chessboard,x_move, y_move,chessboard_size)
+    windstoffAlghorhithm(starting_position_x,starting_position_y,chessboard,x_move, y_move,chessboard_size)
 
 def checkHorseInChessboard(chessboard_size, position_x, position_y, x_move, y_move,chessboard):
     layout = []
@@ -27,11 +27,10 @@ def checkHorseInChessboard(chessboard_size, position_x, position_y, x_move, y_mo
         #print("X: " + str(position_x + x_move[i]), "Y : " + str(position_y+ y_move[i]))
         if moveValidation(position_x + x_move[i], position_y + y_move[i], chessboard_size,chessboard):
             layout.append([position_x + x_move[i], position_y + y_move[i]])
-
     return layout
 
 def moveValidation(position_x, position_y, chessboard_size,chessboard):
-    print("Validating position -> " + str(position_x) + " | " + str(position_y))
+    #print("Validating position -> " + str(position_x) + " | " + str(position_y))
 
     if position_x >= 0 and position_x < chessboard_size and position_y >= 0 and position_y < chessboard_size and chessboard[position_x][position_y] == 0:
         print("VALID: " + str(position_x) +  " AND " + str(position_y))
@@ -40,16 +39,39 @@ def moveValidation(position_x, position_y, chessboard_size,chessboard):
         return False
 
 
-def windstoffAlgorhithm(starting_position_x,starting_position_y,chessboard,x_move, y_move,chessboard_size):
+def windstoffAlghorhithm(starting_position_x,starting_position_y,chessboard,x_move, y_move,chessboard_size):
     position_x = starting_position_x
     position_y = starting_position_y
 
-    positions = checkHorseInChessboard(chessboard_size,position_x,position_y,x_move,y_move,chessboard)
-    print(positions)
+ #   positions = checkHorseInChessboard(chessboard_size,position_x,position_y,x_move,y_move,chessboard)
+    positions = checkHorseInChessboard(chessboard_size, 7, 6 , x_move, y_move, chessboard)
+    #print(positions)
+    minimum = []
+
+    for possible_move in positions:
+        print("MOVE:")
+        print(possible_move)
+        possible_move_positions = checkHorseInChessboard(chessboard_size,possible_move[0],possible_move[1],x_move,y_move,chessboard)
+        print("PMP:")
+        print(possible_move_positions)
+
+        if(len(minimum) > len(possible_move_positions) or len(minimum) == 0):
+            minimum = possible_move_positions
+
+    print("FINAL MINIMUM")
+    print(minimum)
+
+
+
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
     print("Zadaj Veľkosť plochy: \n")
     chessboard_size = int(input())
-    EulerovKon(chessboard_size);
+    EulerovKon(chessboard_size)
